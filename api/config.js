24bb684e16f5201c7f18d66122d678b32a9dc325
palamedeslabs <info+apple@rhod.com>
2026-02-19 22:35:39 -0600

    Add files via upload

---
+++ api/config.js
@@ -0,0 +1,7 @@
+export default function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.status(200).json({
+    supabaseUrl: process.env.SUPABASE_URL,
+    supabaseKey: process.env.SUPABASE_ANON_KEY
+  });
+}
